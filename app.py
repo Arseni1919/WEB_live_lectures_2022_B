5feceacd15d47d36687d86163454628d1a71e207
perchik_air <1919ars@gmail.com>
2022-05-24 19:11:59 +0300

    lecture 2

--- app.py
+++ app.py
@@ -16,15 +16,19 @@ def index_func():
     user_from_db = 'aRIeL'
     user_second_name_from_db = 'Katz'
     return render_template('home_page.html',
-                           user_name=user_from_db,
-                           # user_second_name=user_second_name_from_db,
-                           title='Home')
+                           user_name=user_from_db)
+                           # user_second_name=user_second_name_from_db)
 
 
 @app.route('/about')
 def about_page():
-
-    return render_template('about_page.html')
+    user_info = {'name': 'Jacky', 'second_name': 'Sparrow', 'nickname': 'pirate'}
+    degrees = ['BSc', 'MsD', 'PhD']
+    hobbies = ('drawing', 'books', 'ships', 'chips')
+    return render_template('about_page.html',
+                           # user_info=user_info,
+                           user_degrees=degrees,
+                           hobbies=hobbies)
 
 # def foo():
 #     return 'abc'
